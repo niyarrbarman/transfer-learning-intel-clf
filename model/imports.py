de485fb38a0f3f0448c34d6b78e5dac1dcd9ae8c
Niyar R Barman <113722156+niyarrbarman@users.noreply.github.com>
2022-12-29 17:52:24 +0530

    Create imports.py

---
+++ model/imports.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import timm
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    
+    pass
