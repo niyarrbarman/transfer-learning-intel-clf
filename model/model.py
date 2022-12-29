6a1558651f1143684459e06e240ad30b13e529ef
Niyar R Barman <113722156+niyarrbarman@users.noreply.github.com>
2022-12-29 17:51:39 +0530

    Update model.py

--- model/model.py
+++ model/model.py
@@ -1,11 +1,3 @@
-import torch
-import torch.nn as nn
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import timm
-import numpy as np
-from tqdm import tqdm
-import matplotlib.pyplot as plt
 from imports import *
 
 class TrainEval:
